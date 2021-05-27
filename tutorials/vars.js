2846dda3909f216e6760d132308ea740251c5fd9
Ester <ester_sgomes@hotmail.com>
2021-05-26 22:18:27 -0300

    update firebase config

---
+++ tutorials/vars.js
@@ -0,0 +1,10 @@
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDM07U-SskNMAAZXzPGQ9sgO7R4ztnxg3Q",
+  authDomain: "react-firebase-auth-66700.firebaseapp.com",
+  projectId: "react-firebase-auth-66700",
+  storageBucket: "react-firebase-auth-66700.appspot.com",
+  messagingSenderId: "175160652887",
+  appId: "1:175160652887:web:81ba54549c000ad4495f12",
+  measurementId: "G-KC2LQ6WPCQ"
+};
