952c37f9aaed645378027a82b56487e0ab379c01
Ester <ester_sgomes@hotmail.com>
2021-05-27 19:21:04 -0300

    firebase auth class

--- tutorials/react-firebase-authentication/src/components/Firebase/firebase.js
+++ tutorials/react-firebase-authentication/src/components/Firebase/firebase.js
@@ -1,6 +1,7 @@
-import firebase from 'firebase/app';
+import app from 'firebase/app';
+import 'firebase/auth';
 
-const config = firebase.initializeApp({
+const config = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
@@ -8,6 +9,27 @@ const config = firebase.initializeApp({
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_API_ID,
   measurementId: process.env.REACT_APP_MEASUREMENT_ID
-})
+}
 
-export default config;
+class Firebase {
+  constructor() {
+    app.initializeApp(config);
+
+    this.auth = app.auth();
+  }
+
+  doCreateUserWithEmailAndPassword = (email, password) => 
+    this.auth.createUserWithEmailAndPassword(email, password);
+
+  doSignInWithEmailAndPassword = (email, password) =>
+    this.auth.signInWithEmailAndPassword(email, password);
+
+  doSignOut = () => this.auth.signOut();
+
+  doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+  doPasswordUpdate = password =>
+    this.auth.currentUser.updatePassword(password)
+}
+  
+export default Firebase;
