fd000da59122b18275704224c9314f0307786e1f
Ester <ester_sgomes@hotmail.com>
2021-05-29 21:57:58 -0300

    app game

--- tutorials/react-firebase-authentication/src/components/Firebase/firebase.js
+++ tutorials/react-firebase-authentication/src/components/Firebase/firebase.js
@@ -8,7 +8,6 @@ const config = {
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_API_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 }
 
 class Firebase {
